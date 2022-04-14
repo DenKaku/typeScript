b2c4b74c47be68705c99c844a04ebd0e31bd6265
DenKaku <56074770+DenKaku@users.noreply.github.com>
2022-04-15 00:02:39 +0900

    Update Draw.tsx
    xxx

--- Draw.tsx
+++ Draw.tsx
@@ -1,50 +1,68 @@
-import React from "react";
+import React, { useRef } from "react";
 import { createRoot } from "react-dom/client";
 import { Stage, Rect, Layer } from "react-konva";
 
 const App = () => {
-  const isDrawing = React.useRef(false);
+  const [shouldDraw, setShouldDraw] = React.useState(false);
+  // const [isDrawing, setIsDrawing] = React.useState(false);
+  const isDrawing = useRef(false);
   const [stageX, setStageX] = React.useState(0);
   const [stageY, setStageY] = React.useState(0);
   const [rectWidth, setRectWidth] = React.useState(0);
   const [rectHeight, setRectHeight] = React.useState(0);
 
   const handleMouseDown = (e) => {
-    setStageX(0);
-    setStageY(0);
-    setRectWidth(0);
-    setRectHeight(0);
+    if (shouldDraw) {
+      setStageX(0);
+      setStageY(0);
+      setRectWidth(0);
+      setRectHeight(0);
 
-    isDrawing.current = true;
-    const pos = e.target.getStage().getPointerPosition();
-    setStageX(pos.x);
-    setStageY(pos.y);
-    console.log(pos);
-  };
-
-  const handlerMouseUp = (e) => {
-    const pos = e.target.getStage().getPointerPosition();
-    isDrawing.current = false;
-    setRectWidth(pos.x - stageX);
-    setRectHeight(pos.y - stageY);
-    console.log(pos);
-  };
-
-  const handleOnMouseMove = (e) => {
-    const pos = e.target.getStage().getPointerPosition();
-    if (isDrawing.current) {
-      setRectWidth(pos.x - stageX);
-      setRectHeight(pos.y - stageY);
-
+      isDrawing.current = true;
+      // setIsDrawing(true);
+      const pos = e.target.getStage().getPointerPosition();
+      setStageX(pos.x);
+      setStageY(pos.y);
       console.log(pos);
     }
   };
 
+  const handlerMouseUp = (e) => {
+    if (shouldDraw) {
+      const pos = e.target.getStage().getPointerPosition();
+      isDrawing.current = false;
+      // setIsDrawing(false);
+      setRectWidth(pos.x - stageX);
+      setRectHeight(pos.y - stageY);
+      console.log(pos);
+    }
+  };
+
+  const handleOnMouseMove = (e) => {
+    if (shouldDraw) {
+      const pos = e.target.getStage().getPointerPosition();
+      if (isDrawing.current) {
+        setRectWidth(pos.x - stageX);
+        setRectHeight(pos.y - stageY);
+
+        console.log(pos);
+      }
+    }
+  };
+
+  const onButtonClick = () => {
+    setShouldDraw(true);
+  };
+
+  const onStopDraw = () => {
+    setShouldDraw(false);
+  };
   return (
-    <div className="draw-component">
+    <div style={{ height: 500, width: 500, border: "2px, solid, black" }}>
       <Stage
-        width={window.innerWidth * 0.7}
-        height={window.innerHeight * 0.6}
+        width={300}
+        height={300}
+        style={{ background: "aqua" }}
         onMouseMove={handleOnMouseMove}
         onMouseDown={handleMouseDown}
         onMouseUp={handlerMouseUp}
@@ -61,6 +79,12 @@ const App = () => {
           />
         </Layer>
       </Stage>
+      <button style={{ height: 35, width: 80 }} onClick={onButtonClick}>
+        Draw
+      </button>
+      <button style={{ height: 35, width: 80 }} onClick={onStopDraw}>
+        Stop Draw
+      </button>
     </div>
   );
 };
